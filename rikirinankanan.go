80c665f6dc10ccf2e1ffbcbf43e8717c631017e6
MuhRobai <moh.robai@citra.digital>
2019-12-23 19:17:43 +0700

    hekdey

--- rikirinankanan.go
+++ rikirinankanan.go
@@ -10,15 +10,17 @@ func Moving(value string) {
 	strSplit := strings.Split(payload, ",")
 	word := strSplit[1]
 	movingWord := strSplit[0]
-	start := 1
-	move := false
-	pointer := 0
+	start := 1 // for a moving value
+	move := false // for check moving or not if b pressed
+	pointer := 0 // for b press and space undefine
 	for i := 0; i < len(movingWord) ; i++ {
 		if start < len(word) {
+			// get l value
 			if string(movingWord[i]) == "l" {
 				start++
 			}
 
+			//get w value
 			if string(movingWord[i]) == "w" {
 				for j := start; j < len(word); j++ {
 					if (string(word[j]) == " ") && (start < len(word)) {
@@ -27,17 +29,19 @@ func Moving(value string) {
 					}
 				}
 			}
-
+			//get h value
 			if string(movingWord[i]) == "h" {
 				start--
 			}
 
 			if move == true {
+				//for move to left of space
 				start = start - 3
 				move = false
 			}
 			
 			if string(movingWord[i]) == "b" {
+				//loop for bigget to lower
 				for k := start; k > 0 ; k-- {
 					if (string(word[k]) == " "){
 						start = k + 2
@@ -45,7 +49,7 @@ func Moving(value string) {
 						pointer = start - 3
 						break
 					}
-
+					// if space undefine and b also in pres
 					if pointer == start {
 						start = start - 3
 					}
